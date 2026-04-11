987c982b7ae09f17552961bc9cef956458595c51
MuhammadHamzaKashif <hamzakashifkhanzada@gmail.com>
2026-04-11 19:00:39 +0500

    Added enemy wave management and stat assignment

--- spacewars.asm
+++ spacewars.asm
@@ -51,7 +51,79 @@ Boss ENDS
 
     score       DWORD ?
 
+
+    START_X     DWORD 100
+    START_Y     DWORD 50
+    SPACING_X   DWORD 60
+    SPACING_Y   DWORD 40
+    ROWS        DWORD 3
+    COLS        DWORD 10
+
+    ; stats for enemies in diff rows
+    HEALTH_TOP    DWORD 5
+    HEALTH_MID    DWORD 3
+    HEALTH_BOT    DWORD 1
+
 .CODE
+
+    InitWave PROC
+        pushad
+
+        mov esi, OFFSET enemyArr
+        mov ecx, ROWS
+        mov ebx, START_Y
+
+    L_ROWS:
+        push ecx 
+        mov ecx, COLS  
+        mov edx, START_X    
+
+        L_COLS:
+            ; Set the enemy to active
+            mov [esi].Enemy.isActive, 1
+
+
+            ;       Procedural Stat Assignment
+            ;  - look at stack to get row num
+            ;  - assign stats based on row num
+            mov eax, [esp]          
+            
+            .IF eax == 3            ; Top Row
+                mov [esi].Enemy.enemyType, 2
+                mov eax, HEALTH_TOP
+                mov [esi].Enemy.health, eax
+            .ELSEIF eax == 2        ; Middle Row
+                mov [esi].Enemy.enemyType, 1
+                mov eax, HEALTH_MID
+                mov [esi].Enemy.health, eax
+            .ELSE                   ; Bottom Row
+                mov [esi].Enemy.enemyType, 0
+                mov eax, HEALTH_BOT
+                mov [esi].Enemy.health, eax
+            .ENDIF
+            
+            ; Set the position
+            mov eax, edx
+            mov [esi].Enemy.posx, eax
+            mov eax, ebx
+            mov [esi].Enemy.posy, eax
+            
+            add esi, TYPE Enemy
+            add edx, SPACING_X
+            
+        loop L_COLS 
+
+        add ebx, SPACING_Y
+        pop ecx 
+
+    loop L_ROWS
+
+        popad
+        ret
+
+    InitWave ENDP
+
+
     ; Main shii
     MAIN PROC
         
